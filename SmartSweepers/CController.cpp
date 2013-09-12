dc902e89279de47bf3920cc0d159f2cf91401762
Ben Meier <benmeier42@gmail.com>
2013-09-12 17:49:59 +0200

    Added training screen.

--- SmartSweepers/CController.cpp
+++ SmartSweepers/CController.cpp
@@ -36,7 +36,8 @@ double avgMinesGathered;
 
 bool isFirstTick = true;
 
-
+bool hasTrained = false;
+bool hasRendered = false;
 
 //---------------------------------------constructor---------------------
 //
@@ -59,9 +60,6 @@ CController::CController(HWND hwndMain): m_NumSweepers(CParams::iNumSweepers),
 	{
 		m_vecSweepers.push_back(CMinesweeper());
 	}
-
-	// Setup our MLP network. Training can take a few seconds.
-	mlp.Train();
 
 	//initialize mines in random positions within the application window
 	for (int i=0; i<m_NumMines; ++i)
@@ -146,6 +144,15 @@ void CController::WorldTransform(vector<SPoint> &VBuffer, SVector2D vPos)
 //-------------------------------------------------------------------------
 bool CController::Update()
 {
+	if (!hasTrained)
+	{
+		if (hasRendered)
+		{
+			mlp.Train();
+			hasTrained = true;
+		}
+
+	}
 	//run the sweepers through CParams::iNumTicks amount of cycles. During
 	//this loop each sweeper is constantly updated with the appropriate
 	//information from its surroundings. The output from the learning algorithm is obtained
@@ -240,113 +247,126 @@ bool CController::Update()
 //----------------------------------------------------------------------------------
 void CController::Render(HDC surface)
 {
-
-	//do not render if running at accelerated speed
-	if (!m_bFastRender)
+	if ( hasTrained)
 	{
-		//keep a record of the old pen
-		m_OldPen = (HPEN)SelectObject(surface, m_GreenPen);
+
+		//do not render if running at accelerated speed
+		if (!m_bFastRender)
+		{
+			//keep a record of the old pen
+			m_OldPen = (HPEN)SelectObject(surface, m_GreenPen);
 		
-		//render the mines
-		for (int i=0; i<m_NumMines+m_NumSuperMines+m_NumRocks; ++i)
-		{
-			if ( m_vecObjects[i].getType() == CCollisionObject::Mine)
+			//render the mines
+			for (int i=0; i<m_NumMines+m_NumSuperMines+m_NumRocks; ++i)
 			{
-				SelectObject(surface, m_PinkPen);
-			}
-			else if ( m_vecObjects[i].getType() == CCollisionObject::Rock)
-			{
-				SelectObject(surface, m_BrownPen );
-			}
-			else if ( m_vecObjects[i].getType() == CCollisionObject::SuperMine)
-			{
-				SelectObject(surface, m_RedPen);
-			}
-			//grab the vertices for the mine shape
-			vector<SPoint> mineVB = m_MineVB;
+				if ( m_vecObjects[i].getType() == CCollisionObject::Mine)
+				{
+					SelectObject(surface, m_PinkPen);
+				}
+				else if ( m_vecObjects[i].getType() == CCollisionObject::Rock)
+				{
+					SelectObject(surface, m_BrownPen );
+				}
+				else if ( m_vecObjects[i].getType() == CCollisionObject::SuperMine)
+				{
+					SelectObject(surface, m_RedPen);
+				}
+				//grab the vertices for the mine shape
+				vector<SPoint> mineVB = m_MineVB;
 
-			WorldTransform(mineVB, m_vecObjects[i].getPosition());
+				WorldTransform(mineVB, m_vecObjects[i].getPosition());
 
-			//draw the mines
-			MoveToEx(surface, (int)mineVB[0].x, (int)mineVB[0].y, NULL);
+				//draw the mines
+				MoveToEx(surface, (int)mineVB[0].x, (int)mineVB[0].y, NULL);
 
-			LineTo(surface, (int)mineVB[1].x, (int)mineVB[1].y);
-			LineTo(surface, (int)mineVB[2].x, (int)mineVB[2].y);			
-			LineTo(surface, (int)mineVB[3].x, (int)mineVB[3].y);					
-			LineTo(surface, (int)mineVB[0].x, (int)mineVB[0].y);
+				LineTo(surface, (int)mineVB[1].x, (int)mineVB[1].y);
+				LineTo(surface, (int)mineVB[2].x, (int)mineVB[2].y);			
+				LineTo(surface, (int)mineVB[3].x, (int)mineVB[3].y);					
+				LineTo(surface, (int)mineVB[0].x, (int)mineVB[0].y);
 
-			LineTo(surface, (int)mineVB[2].x, (int)mineVB[2].y);
-			MoveToEx(surface, (int)mineVB[1].x, (int)mineVB[1].y, NULL);
-			LineTo(surface, (int)mineVB[3].x, (int)mineVB[3].y);
+				LineTo(surface, (int)mineVB[2].x, (int)mineVB[2].y);
+				MoveToEx(surface, (int)mineVB[1].x, (int)mineVB[1].y, NULL);
+				LineTo(surface, (int)mineVB[3].x, (int)mineVB[3].y);
 			
-		}
+			}
        		
-		//we want some sweepers displayed in red
-		SelectObject(surface, m_RedPen);
+			//we want some sweepers displayed in red
+			SelectObject(surface, m_RedPen);
 
-		//render the sweepers
-		for (int i=0; i<m_NumSweepers; i++)
-		{
-			if (i == CParams::iNumElite)
+			//render the sweepers
+			for (int i=0; i<m_NumSweepers; i++)
 			{
-				SelectObject(surface, m_OldPen);
-			}
+				if (i == CParams::iNumElite)
+				{
+					SelectObject(surface, m_OldPen);
+				}
       
-			//grab the sweeper vertices
-			vector<SPoint> sweeperVB = m_SweeperVB;
+				//grab the sweeper vertices
+				vector<SPoint> sweeperVB = m_SweeperVB;
 
-			//transform the vertex buffer
-			m_vecSweepers[i].WorldTransform(sweeperVB);
+				//transform the vertex buffer
+				m_vecSweepers[i].WorldTransform(sweeperVB);
 
-			//draw the sweeper left track
-			MoveToEx(surface, (int)sweeperVB[0].x, (int)sweeperVB[0].y, NULL);
+				//draw the sweeper left track
+				MoveToEx(surface, (int)sweeperVB[0].x, (int)sweeperVB[0].y, NULL);
 
-			for (int vert=1; vert<4; ++vert)
-			{
-				LineTo(surface, (int)sweeperVB[vert].x, (int)sweeperVB[vert].y);
+				for (int vert=1; vert<4; ++vert)
+				{
+					LineTo(surface, (int)sweeperVB[vert].x, (int)sweeperVB[vert].y);
+				}
+
+				LineTo(surface, (int)sweeperVB[0].x, (int)sweeperVB[0].y);
+
+				//draw the sweeper right track
+				MoveToEx(surface, (int)sweeperVB[4].x, (int)sweeperVB[4].y, NULL);
+
+				for (int vert=5; vert<8; ++vert)
+				{
+					LineTo(surface, (int)sweeperVB[vert].x, (int)sweeperVB[vert].y);
+				}
+
+				LineTo(surface, (int)sweeperVB[4].x, (int)sweeperVB[4].y);
+
+				MoveToEx(surface, (int)sweeperVB[8].x, (int)sweeperVB[8].y, NULL);
+				LineTo(surface, (int)sweeperVB[9].x, (int)sweeperVB[9].y);
+
+				MoveToEx(surface, (int)sweeperVB[10].x, (int)sweeperVB[10].y, NULL);
+
+				for (int vert=11; vert<16; ++vert)
+				{
+					LineTo(surface, (int)sweeperVB[vert].x, (int)sweeperVB[vert].y);
+				}
+
 			}
 
-			LineTo(surface, (int)sweeperVB[0].x, (int)sweeperVB[0].y);
-
-			//draw the sweeper right track
-			MoveToEx(surface, (int)sweeperVB[4].x, (int)sweeperVB[4].y, NULL);
-
-			for (int vert=5; vert<8; ++vert)
-			{
-				LineTo(surface, (int)sweeperVB[vert].x, (int)sweeperVB[vert].y);
-			}
-
-			LineTo(surface, (int)sweeperVB[4].x, (int)sweeperVB[4].y);
-
-			MoveToEx(surface, (int)sweeperVB[8].x, (int)sweeperVB[8].y, NULL);
-			LineTo(surface, (int)sweeperVB[9].x, (int)sweeperVB[9].y);
-
-			MoveToEx(surface, (int)sweeperVB[10].x, (int)sweeperVB[10].y, NULL);
-
-			for (int vert=11; vert<16; ++vert)
-			{
-				LineTo(surface, (int)sweeperVB[vert].x, (int)sweeperVB[vert].y);
-			}
+			//put the old pen back
+			SelectObject(surface, m_OldPen);
 
 		}
+		else
+		{
+			PlotStats(surface);
+		}
 
-		//put the old pen back
-		SelectObject(surface, m_OldPen);
-
+	
+		//render the stats
+		string s = "Iteration:          " + itos(m_iIterations);
+		TextOut(surface, 5, 0, s.c_str(), s.size());
+	
+		string s2 = "Progress:   " + itos(m_iTicks) + "/" + itos(CParams::iNumTicks) + " ticks";
+		TextOut(surface, 5, 20, s2.c_str(), s2.size());
 	}
 	else
 	{
-		PlotStats(surface);
+
+		string s = "Training...";
+		TextOut(surface, 5, 0, s.c_str(), s.size());
+
+		string s = "This generally takes under 10 seconds.";
+		TextOut(surface, 5, 20, s.c_str(), s.size());
+
+		hasRendered = true;
 	}
-
-	
-	//render the stats
-	string s = "Iteration:          " + itos(m_iIterations);
-	TextOut(surface, 5, 0, s.c_str(), s.size());
-	
-	string s2 = "Progress:   " + itos(m_iTicks) + "/" + itos(CParams::iNumTicks) + " ticks";
-	TextOut(surface, 5, 20, s2.c_str(), s2.size());
-
 }
 //--------------------------PlotStats-------------------------------------
 //
